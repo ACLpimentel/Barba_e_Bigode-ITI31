53f9af3cd554a6d5b6aab41a4cda060ac7ef1130
regilanmeira <regilan@hotmail.com>
2023-12-14 08:12:00 -0300

    Atualização do script de base dados com o backup feito no PHPMyAdmin

--- scrit_bd.sql
+++ scrit_bd.sql
@@ -1,28 +1,127 @@
-CREATE DATABASE barba_bigode;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 14/12/2023 às 12:09
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
 
-USE barba_bigode;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE servico
-(
-	idServiço INT PRIMARY KEY AUTO_INCREMENT ,
-	descricao VARCHAR(50) NOT NULL,
-	preco FLOAT
-)
 
-CREATE TABLE filial
-(
-	idFilial INT PRIMARY KEY AUTO_INCREMENT,
-	descricao varchar(50) NOT NULL,
-	localizacao VARCHAR(50) NOT NULL,
-	responsavel VARCHAR(50) NOT NULL
-)
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE atendimento
-(
-	idAtendiemnto INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50) NOT NULL,
-	idServiço INT FOREIGN KEY (servico),
-	idFilial INT FOREIGN KEY (filial),
-	dataAtendimento DATE NOT NULL,
-	horarioPreferencial VARCHAR(20)
-)
+--
+-- Banco de dados: `barba_bigode`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `atendimento`
+--
+
+CREATE TABLE `atendimento` (
+  `idAtendiemnto` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `idServiço` int(11) DEFAULT NULL,
+  `idFilial` int(11) DEFAULT NULL,
+  `dataAtendimento` date NOT NULL,
+  `horarioPreferencial` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `filial`
+--
+
+CREATE TABLE `filial` (
+  `idFilial` int(11) NOT NULL,
+  `descricao` varchar(50) NOT NULL,
+  `localizacao` varchar(50) NOT NULL,
+  `responsavel` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `filial`
+--
+
+INSERT INTO `filial` (`idFilial`, `descricao`, `localizacao`, `responsavel`) VALUES
+(1, 'Corte masculino', 'Centro', 'regilan'),
+(2, 'Corte FEMININO', 'Centro', 'regilan');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `servico`
+--
+
+CREATE TABLE `servico` (
+  `idServiço` int(11) NOT NULL,
+  `descricao` varchar(50) NOT NULL,
+  `preco` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `servico`
+--
+
+INSERT INTO `servico` (`idServiço`, `descricao`, `preco`) VALUES
+(1, 'Unha', 15),
+(2, 'Sombracelhas', 20);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `atendimento`
+--
+ALTER TABLE `atendimento`
+  ADD PRIMARY KEY (`idAtendiemnto`);
+
+--
+-- Índices de tabela `filial`
+--
+ALTER TABLE `filial`
+  ADD PRIMARY KEY (`idFilial`);
+
+--
+-- Índices de tabela `servico`
+--
+ALTER TABLE `servico`
+  ADD PRIMARY KEY (`idServiço`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `atendimento`
+--
+ALTER TABLE `atendimento`
+  MODIFY `idAtendiemnto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `filial`
+--
+ALTER TABLE `filial`
+  MODIFY `idFilial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `servico`
+--
+ALTER TABLE `servico`
+  MODIFY `idServiço` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
